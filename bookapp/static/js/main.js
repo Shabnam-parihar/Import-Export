c893259bfaa04bb1981011b7f320f4f93980916c
shabnam parihar <shabnampariharp64@gmail.com>
2026-01-13 23:55:20 +0530

    remove js

--- bookapp/static/js/main.js
+++ bookapp/static/js/main.js
@@ -33,21 +33,5 @@ document.addEventListener('click', function(e) {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburger = document.getElementById('hamburger');
-  const mobileMenu = document.getElementById('mobileMenu');
-  const overlay = document.getElementById('menuOverlay');
-
-  hamburger.addEventListener('click', function (e) {
-    e.stopPropagation();
-    mobileMenu.classList.toggle('active');
-    overlay.classList.toggle('active');
-  });
-
-  overlay.addEventListener('click', function () {
-    mobileMenu.classList.remove('active');
-    overlay.classList.remove('active');
-  });
-});
 
 
