0629c4aa63637eb8e6c52f4f29e166c3944d1501
shabnam parihar <shabnampariharp64@gmail.com>
2026-01-13 23:47:21 +0530

    change photo email color

--- bookapp/static/js/main.js
+++ bookapp/static/js/main.js
@@ -32,4 +32,17 @@ document.addEventListener('click', function(e) {
         }
     });
 });
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', (e) => {
+  e.stopPropagation(); // prevent immediate close
+  mobileMenu.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+  if (!mobileMenu.contains(e.target) && !hamburger.contains(e.target)) {
+    mobileMenu.classList.remove('active');
+  }
+});
 
